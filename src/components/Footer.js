e39d54fdb5c70828601d682248238f308a322706
Bram Borggreve <beeman@beeman.nl>
2023-09-25 18:37:54 +0100

    fix: clean up Solana logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,13 @@ export function Footer() {
     <div className="footer p-5">
       <div className="d-flex align-items-center justify-content-between">
         <div>
-          <img src={solanaImg} alt="Solana" />
-          <p className="primary-text">Made possible on the Solana network</p>
-          <p className="primary-text">© 2023 Dean's List DAO. All rights reserved.</p>
+          <img src={solanaImg} alt="Solana" style={{ height: 26 }} />
+          <p className="primary-text" style={{ marginTop: 12 }}>
+            Made possible on the Solana network
+          </p>
+          <p className="primary-text" style={{ marginTop: 4 }}>
+            © 2023 Dean's List DAO. All rights reserved.
+          </p>
         </div>
         <div>
           <FooterLinks />
