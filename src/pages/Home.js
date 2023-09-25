761df09ae27185a4bd60fe771ff344065ee307d4
Bram Borggreve <beeman@beeman.nl>
2023-09-25 18:43:04 +0100

    refactor: optimize homepage links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,11 @@ export function Home() {
       <div style={{ textAlign: 'center' }}>
         <iframe
           className="iframe-map"
-          style={{ width: '100%', overflow: 'hidden' }}
+          style={{
+            width: 1280,
+            height: 1000,
+            overflow: 'hidden',
+          }}
           src="https://lookerstudio.google.com/embed/reporting/6607ecd0-33e5-4df0-b44b-6df9feb65682/page/pFjXD"
           title="Dean's List Map"
           frameborder="0"
@@ -16,19 +20,15 @@ export function Home() {
       </div>
       <div className="text-center">
         <p className="heading-text">A Network State Of Web3 Powerusers</p>
-        <div className="mb-3">
-          <Link to="/join-us">
-            <button className="primary-button">JOIN US</button>
+        <div style={{ display: 'flex', justifyContent: 'center', gap: 32, margin: '32px 32px' }}>
+          <Link to="/join-us" className="primary-button">
+            JOIN US
           </Link>
-        </div>
-        <div className="mb-3">
-          <Link to="/hire-us">
-            <button className="primary-button">HIRE US</button>
+          <Link to="/hire-us" className="primary-button">
+            HIRE US
           </Link>
-        </div>
-        <div>
-          <Link to="/partner-with-us">
-            <button className="primary-button">PARTNER WITH US</button>
+          <Link to="/partner-with-us" className="primary-button">
+            PARTNER WITH US
           </Link>
         </div>
       </div>
